4d13681d8ee7c7240583b2d1e01a5991f7864bad
Mike Criollo <mkcriollo@gmail.com>
2022-01-12 12:02:32 -0500

    create search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
+/*
+Notes: 
+- once context is finish 
+- onSubmit we will call a fetch function
+- on input change we will be setting the state for the query
+*/
 const Search = () => {
-  return <h2>search component</h2>;
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form>
+          <div className="form-control">
+            <MdSearch />
+            <input type="text" placeholder="Enter Github User"></input>
+            <button type="submit">Search</button>
+          </div>
+        </form>
+        <h3>Requests: 56 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
